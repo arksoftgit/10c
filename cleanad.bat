fa8a47500018b742f68950955537e22a9d0d9a64
Doug Sorensen <arksoft@comcast.net>
2014-09-23 10:37:47 -0400

    Sync with Kelly 2014.09.23

--- cleanad.bat
+++ cleanad.bat
@@ -1,27 +1,46 @@
-qrw ad.rpt /c.obj;x /r
-qrw ad.rpt /c.sbr;x /r
-qrw ad.rpt /c.pch;x /r
-qrw ad.rpt /c.pdb;x /r
-qrw ad.rpt /c.log;x /r
-qrw ad.rpt /c.bsc;x /r
-qrw ad.rpt /c.idb;x /r
-qrw ad.rpt /c.dep;x /r
-qrw ad.rpt /c.exp;x /r
-qrw ad.rpt /c.lib;x /r
-qrw ad.rpt /c.dll;x /r
-qrw ad.rpt /c.DLL;x /r
-qrw ad.rpt /c.exe;x /r
-qrw ad.rpt /c.ocx;x /r
-qrw ad.rpt /c.tlb;x /r
-qrw ad.rpt /c.tlb;x /r
-qrw ad.rpt /c.bat;x /r
-qrw ad.rpt /c.ncb;x /r
-qrw ad.rpt /c.opt;x /r
-qrw ad.rpt /c.sln;x /r
-qrw ad.rpt /cfound;x /r
-qrw ad.rpt /c.vcxproj;x /r
-qrw ad.rpt /c.lastbuildstate;x /r
-qrw ad.rpt /c.tlog;x /r
+qrw ad.rpt /r.asm
+qrw ad.rpt /r.bat
+qrw ad.rpt /r.bsc
+qrw ad.rpt /r.dep
+qrw ad.rpt /r.dks
+qrw ad.rpt /r.DKS
+qrw ad.rpt /r.dll
+qrw ad.rpt /r.DLL
+qrw ad.rpt /r.DXX
+qrw ad.rpt /r.err
+qrw ad.rpt /r.ERR
+qrw ad.rpt /r.exe
+qrw ad.rpt /r.exp
+qrw ad.rpt /r.idb
+qrw ad.rpt /r.ilk
+qrw ad.rpt /r.lastbuildstate
+qrw ad.rpt /r.ler
+qrw ad.rpt /r.LER
+qrw ad.rpt /r.lib
+qrw ad.rpt /r.log
+qrw ad.rpt /r.map
+qrw ad.rpt /r.MAP
+qrw ad.rpt /r.ncb
+qrw ad.rpt /r"ot found"
+qrw ad.rpt /r.obj
+qrw ad.rpt /r.ocx
+qrw ad.rpt /r.opt
+qrw ad.rpt /r.pch
+qrw ad.rpt /r.pdb
+qrw ad.rpt /r.PDB
+qrw ad.rpt /r.RST
+qrw ad.rpt /r.sbr
+qrw ad.rpt /r.tlb
+qrw ad.rpt /r.tmp
+qrw ad.rpt /r.sln
+qrw ad.rpt /r.tlog
+qrw ad.rpt /r.vcxproj
+qrw ad.rpt /r.xpg
+qrw ad.rpt /r.XPG
+qrw ad.rpt /r.xpj
+qrw ad.rpt /r.XPJ
+qrw ad.rpt /r.xsv
+qrw ad.rpt /rz__temp
 type ad.rpt
 @echo .
 
